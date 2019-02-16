bd48afb1b5541c11ced3ef47e7fca0ed8305fae3
nikita lalwani <nikitalalwani@nikitas-macbook-air.local>
2019-02-16 13:30:19 -0500

    Initial Commit

---
+++ MovieRentalSystem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MovieRentalSystem
+//
+//  Created by nikita lalwani on 2/16/19.
+//  Copyright © 2019 nikita lalwani. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
